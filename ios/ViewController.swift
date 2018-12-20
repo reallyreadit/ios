dc100fdb9a8ca7884b9de860bad3231d7beb4d06
jeff <jeff@jeffcamera.com>
2018-12-19 19:14:49 -0500

    Initial Commit

---
+++ ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Jeff Camera on 12/19/18.
+//  Copyright © 2018 reallyread.it, inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
