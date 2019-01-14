1fd24af5dc7dec3ce99aba9c6f2e60ae7b064196
jeff <jeff@jeffcamera.com>
2019-01-13 20:11:06 -0500

    basic starring working in share-extension with default template

--- share-extension/ShareViewController.swift
+++ share-extension/ShareViewController.swift
@@ -1,8 +1,56 @@
 import UIKit
 import Social
+import WebKit
 
-class ShareViewController: SLComposeServiceViewController {
-
+class ShareViewController: SLComposeServiceViewController, MessageWebViewDelegate {
+    private var url: URL?
+    private var webView: MessageWebView!
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        let config = WKWebViewConfiguration()
+        ArticleReading.addContentScript(forConfiguration: config)
+        webView = MessageWebView(webViewConfig: config)
+        webView.delegate = self
+    }
+    func onMessage(message: (type: String, data: Any?), callbackId: Int?) {
+        switch message.type {
+        case "registerContentScript":
+            print("registerContentScript")
+            webView.sendResponse(
+                data: ContentScriptInitData(
+                    config: ContentScriptConfig(
+                        idleReadRate: 500,
+                        pageOffsetUpdateRate: 3000,
+                        readStateCommitRate: 3000,
+                        readWordRate: 100
+                    ),
+                    loadPage: true,
+                    parseMetadata: true,
+                    parseMode: "analyze",
+                    showOverlay: false,
+                    sourceRules: []
+                ),
+                callbackId: callbackId!
+            )
+        case "registerPage":
+            print("registerPage")
+            APIServer.postJson(
+                path: "/Extension/GetUserArticle",
+                data: PageParseResult(message.data as! [String: Any]),
+                onSuccess: {
+                    (result: ArticleLookupResult) in
+                    print("starred: " + result.userArticle.title)
+                },
+                onError: {
+                    _ in
+                    print("error starring article")
+                }
+            )
+        default:
+            return
+        }
+    }
+    
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
         return true
@@ -13,7 +61,9 @@ class ShareViewController: SLComposeServiceViewController {
     
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         print("didSelectPost()")
-        let config = URLSessionConfiguration.default
+        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        
+        /*let config = URLSessionConfiguration.default
         config.httpCookieStorage = HTTPCookieStorage.sharedCookieStorage(
             forGroupContainerIdentifier: "group.it.reallyread"
         )
@@ -36,7 +86,47 @@ class ShareViewController: SLComposeServiceViewController {
                     self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
                 }
             )
-            .resume()
+            .resume()*/
+    }
+    
+    override func viewDidLoad() {
+        if
+            let item = extensionContext?.inputItems.first as? NSExtensionItem,
+            let urlAttachment = item.attachments?.first(where: {
+                attachment in attachment.hasItemConformingToTypeIdentifier("public.url")
+            })
+        {
+            urlAttachment.loadItem(
+                forTypeIdentifier: "public.url",
+                options: nil,
+                completionHandler: {
+                    [weak self] value, error in
+                    if
+                        let self = self,
+                        let url = value as? URL
+                    {
+                        self.url = url
+                        ArticleReading.fetchArticle(
+                            url: url,
+                            onSuccess: {
+                                [weak self] content in
+                                if let self = self {
+                                    DispatchQueue.main.async {
+                                        self.webView.view.loadHTMLString(
+                                            content as String,
+                                            baseURL: url
+                                        )
+                                    }
+                                }
+                            },
+                            onError: {
+                                print("Error loading article.")
+                            }
+                        )
+                    }
+                }
+            )
+        }
     }
 
     override func configurationItems() -> [Any]! {
