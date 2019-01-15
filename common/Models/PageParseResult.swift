4c5e5d588bab4ee93650d00a9b52627016d97305
jeff <jeff@jeffcamera.com>
2019-01-15 18:38:37 -0500

    updated PageParseResult with star param

--- common/Models/PageParseResult.swift
+++ common/Models/PageParseResult.swift
@@ -1,17 +1,18 @@
 import Foundation
 
 struct PageParseResult: Codable {
-    init(_ data: [String: Any]) {
+    init(contentScriptData data: [String: Any], star: Bool = false) {
         url = data["url"] as! String
         number = data["number"] as? Int
         wordCount = data["wordCount"] as! Int
         readableWordCount = data["readableWordCount"] as! Int
         article = ArticleParseResult(data["article"] as! [String: Any])
+        self.star = star
     }
     let url: String
     let number: Int?
     let wordCount: Int
     let readableWordCount: Int
     let article: ArticleParseResult
-    let star = true
+    let star: Bool
 }
