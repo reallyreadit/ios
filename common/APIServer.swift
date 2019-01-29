333c46c71e5a68c83f6952c3e9fd798f19383aa6
jeff <jeff@jeffcamera.com>
2019-01-29 13:47:27 -0500

    added getJson to APIServer

--- common/APIServer.swift
+++ common/APIServer.swift
@@ -8,22 +8,18 @@ struct APIServer {
         )
         return URLSession(configuration: config)
     }()
-    static func postJson<TData: Encodable, TResult: Decodable>(
-        path: String,
-        data: TData?,
+    private static func createURL(fromPath path: String) -> URL {
+        return URL(
+            string: (Bundle.main.infoDictionary!["RRITAPIServerURL"] as! String)
+                .trimmingCharacters(in: ["/"]) + path
+        )!
+    }
+    private static func sendRequest<TResult: Decodable>(
+        request: URLRequest,
         onSuccess: @escaping (_: TResult) -> Void,
         onError: @escaping (_: Error?) -> Void
     ) {
-        var request = URLRequest(
-            url: URL(
-                string: (Bundle.main.infoDictionary!["RRITAPIServerURL"] as! String)
-                    .trimmingCharacters(in: ["/"]) + path
-            )!
-        )
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try! JSONEncoder().encode(data)
-        APIServer.urlSession.dataTask(
+        urlSession.dataTask(
             with: request,
             completionHandler: {
                 (data, response, error) in
@@ -47,5 +43,47 @@ struct APIServer {
             }
         )
         .resume()
+    }
+    static func getJson<TResult: Decodable>(
+        path: String,
+        queryItems: URLQueryItem...,
+        onSuccess: @escaping (_: TResult) -> Void,
+        onError: @escaping (_: Error?) -> Void
+    ) {
+        var url = createURL(fromPath: path)
+        if
+            queryItems.count > 0,
+            var components = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        {
+            if components.queryItems == nil {
+                components.queryItems = queryItems
+            } else {
+                components.queryItems!.append(contentsOf: queryItems)
+            }
+            url = components.url ?? url
+        }
+        sendRequest(
+            request: URLRequest(url: url),
+            onSuccess: onSuccess,
+            onError: onError
+        )
+    }
+    static func postJson<TData: Encodable, TResult: Decodable>(
+        path: String,
+        data: TData?,
+        onSuccess: @escaping (_: TResult) -> Void,
+        onError: @escaping (_: Error?) -> Void
+    ) {
+        var request = URLRequest(url: createURL(fromPath: path))
+        request.httpMethod = "POST"
+        if data != nil {
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpBody = try! JSONEncoder().encode(data)
+        }
+        sendRequest(
+            request: request,
+            onSuccess: onSuccess,
+            onError: onError
+        )
     }
 }
