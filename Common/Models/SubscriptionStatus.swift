4b0cd247e0d6d206c1a9419ba0380ca4278f66ab
jeff <jeff@jeffcamera.com>
2021-03-04 10:30:35 -0500

    split incomplete subscription status

--- Common/Models/SubscriptionStatus.swift
+++ Common/Models/SubscriptionStatus.swift
@@ -3,16 +3,17 @@ import Foundation
 enum SubscriptionStatusType: Int, Codable {
     case
         neverSubscribed = 1,
-        incomplete = 2,
-        active = 3,
-        lapsed = 4
+        paymentConfirmationRequired = 2,
+        paymentFailed = 3,
+        active = 4,
+        lapsed = 5
 }
 struct SubscriptionStatus: Codable {
     let type: SubscriptionStatusType
     let isUserFreeForLife: Bool
     let provider: SubscriptionProvider?
     let price: SubscriptionPrice?
-    let requiresConfirmation: Bool?
+    let invoiceId: String?
     let currentPeriodBeginDate: Date?
     let currentPeriodEndDate: Date?
     let lastPeriodEndDate: Date?
