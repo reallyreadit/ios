245c75867955826af8a588865af9e0d88f28e08d
jeff <jeff@jeffcamera.com>
2021-07-22 15:46:23 -0400

    fixed user-agent formatting

--- common/ArticleProcessing/ArticleProcessing.swift
+++ common/ArticleProcessing/ArticleProcessing.swift
@@ -56,7 +56,7 @@ struct ArticleProcessing {
         )
         // use desktop user agent
         request.setValue(
-            "'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
             forHTTPHeaderField: "User-Agent"
         )
         // use TempHTTPCookieStorage
