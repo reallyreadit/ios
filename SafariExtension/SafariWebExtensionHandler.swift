aba1dc454decd2162b31aa66654ffd2a156d3f52
jeff <jeff@jeffcamera.com>
2021-08-14 16:50:30 -0400

    implemented native messaging protocol

--- SafariExtension/SafariWebExtensionHandler.swift
+++ SafariExtension/SafariWebExtensionHandler.swift
@@ -1,11 +1,3 @@
-//
-//  SafariWebExtensionHandler.swift
-//  safari-extension
-//
-//  Created by Jeff Camera on 9/19/20.
-//  Copyright © 2020 reallyread.it, inc. All rights reserved.
-//
-
 import SafariServices
 import os.log
 
@@ -15,13 +7,27 @@ class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
 
 	func beginRequest(with context: NSExtensionContext) {
         let item = context.inputItems[0] as! NSExtensionItem
-        let message = item.userInfo?[SFExtensionMessageKey]
-        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
-
-        let response = NSExtensionItem()
-        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
-
-        context.completeRequest(returningItems: [response], completionHandler: nil)
+        let message = item.userInfo?[SFExtensionMessageKey] as! [String: String]
+        
+        var readupAppURL = URLComponents(string: "readup://read")!
+        readupAppURL.queryItems = [
+            URLQueryItem(name: "url", value: message["url"])
+        ]
+        
+        NSWorkspace.shared.open(
+            readupAppURL.url!,
+            configuration: NSWorkspace.OpenConfiguration(),
+            completionHandler: {
+                app, error in
+                let response = NSExtensionItem()
+                response.userInfo = [
+                    SFExtensionMessageKey: [
+                        "status": app != nil ? 0 : 1
+                    ]
+                ]
+                context.completeRequest(returningItems: [response], completionHandler: nil)
+            }
+        )
     }
     
 }
