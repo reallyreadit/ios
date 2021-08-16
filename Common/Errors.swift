9592802f8e6bc28c27681889d3abe5fc4cb52469
jeff <jeff@jeffcamera.com>
2021-08-16 16:59:51 -0400

    added BrowserExtensionAppErrorType

--- Common/Errors.swift
+++ Common/Errors.swift
@@ -8,6 +8,12 @@ enum AppStoreErrorType: String {
         receiptNotFound = "https://docs.readup.com/errors/app-store/receipt-not-found",
         receiptRequestFailed = "https://docs.readup.com/errors/app-store/receipt-request-failed"
 }
+enum BrowserExtensionAppErrorType: String {
+    case
+        messageParsingFailed = "https://docs.readup.com/errors/browser-extension-app/message-parsing-failed",
+        readupProtocolFailed = "https://docs.readup.com/errors/browser-extension-app/readup-protocol-failed",
+        unexpectedMessageType = "https://docs.readup.com/errors/browser-extension-app/unexpected-message-type"
+}
 enum GeneralErrorType: String {
     case
         exception = "https://docs.readup.com/errors/general/exception"
