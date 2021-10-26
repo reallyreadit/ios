0c0c0333193365e8b94cc211939d4472dcb0f2e0
jeff <jeff@jeffcamera.com>
2021-10-26 16:49:24 -0400

    add additional headers to impersonate browser

--- common/ArticleProcessing/ArticleProcessing.swift
+++ common/ArticleProcessing/ArticleProcessing.swift
@@ -98,6 +98,20 @@ private let hostSpecificRequestPreProcessors: [ String: RequestPreProcessor ] = 
         )
     }
 ]
+private let browserRequestHeaders = [
+    "sec-ch-ua": "\"Chromium\";v=\"94\", \"Google Chrome\";v=\"94\", \";Not A Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-User": "?1",
+    "Sec-Fetch-Dest": "document",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9"
+]
 private func processArticleContent(
     content: NSMutableString,
     mode: ArticleProcessingMode
@@ -145,11 +159,10 @@ struct ArticleProcessing {
                 )!
             )
         )
-        // use desktop user agent
-        request.setValue(
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-            forHTTPHeaderField: "User-Agent"
-        )
+        // impersonate chrome on windows 10 desktop
+        for header in browserRequestHeaders {
+            request.setValue(header.value, forHTTPHeaderField: header.key)
+        }
         // use TempHTTPCookieStorage
         let cookieJar = TempHTTPCookieStorage()
         let sessionConfig = URLSessionConfiguration.default
