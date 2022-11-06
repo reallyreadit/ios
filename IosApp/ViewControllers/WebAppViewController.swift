daa9368145f0fbf05d3ceb5c464f59c80d3444d3
jeff <jeff@jeffcamera.com>
2022-11-06 16:53:07 -0500

    convert reallyread.it & readup.com to readup.org

--- IosApp/ViewControllers/WebAppViewController.swift
+++ IosApp/ViewControllers/WebAppViewController.swift
@@ -26,11 +26,11 @@ private func prepareURL(_ url: URL) -> URL {
     // create components from the provided url or fall back to using the web server url
     var components = URLComponents(url: url, resolvingAgainstBaseURL: true) ??
         URLComponents(url: SharedBundleInfo.webServerURL, resolvingAgainstBaseURL: true)!
-    // convert reallyread.it urls to readup.com
+    // convert reallyread.it and readup.com urls to readup.org
     components.host = components.host?.replacingOccurrences(
-        of: "reallyread.it",
+        of: "(reallyread\\.it|readup\\.com)",
         with: SharedBundleInfo.webServerURL.host!,
-        options: [.caseInsensitive]
+        options: [.caseInsensitive, .regularExpression]
     )
     // verify that the host matches the web server host
     if components.host != SharedBundleInfo.webServerURL.host {
